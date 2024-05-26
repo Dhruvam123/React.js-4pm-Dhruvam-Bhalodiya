ae257682c62550b1be99cbd4df81f3ea76efcdb1
Dhruvam123 <109137396+Dhruvam123@users.noreply.github.com>
2024-05-26 21:28:18 +0530

    successfully commited

--- Assignment/Module-3/Question10/src/App.jsx
+++ Assignment/Module-3/Question10/src/App.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import './assets/style.css'
+
 
 function App() {
   const strings = ['Use Array.map', 'Not a for loop', 'Give each item a unique key', 'Avoid using array index as the key'];
@@ -9,19 +10,15 @@ function App() {
 
   return (
     <>
-    <div style={{margin:"auto",width:"600px",alignItems:"center",backgroundColor:"green",height:"250px"}}>
+    <div className='wrapper'>
     <h2 className='top'>React way to render a list</h2>
-    <div >
-    <ul style={{listStyle:"none",marginRight:"25px"}}>
+    <div className='divlist' >
+    <ul className='list'>
       {strings.map((str) => (
-        <li style={{backgroundColor:"white",display:"block",marginBottom:"5px"}} key={str}>{str}</li>
+        <li key={str}>{str}</li>
       ))}
     </ul>
     </div>
-    
-
-
-
     </div>
     
       
