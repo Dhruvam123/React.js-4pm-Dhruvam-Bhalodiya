bb4e8ba2986f0ff8ca18e54939ece4fa8c148542
Dhruvam123 <109137396+Dhruvam123@users.noreply.github.com>
2024-06-21 18:19:27 +0530

    successfully commited

--- Assessment/Test/Crud_Operation/src/EditTask.jsx
+++ Assessment/Test/Crud_Operation/src/EditTask.jsx
@@ -1,51 +1,64 @@
 import React,{useState,useRef,useEffect} from 'react'
 import axios from 'axios';
+import {useNavigate,useParams} from 'react-router-dom'
 import {Container,Form} from 'react-bootstrap'
 import Swal from 'sweetalert2';
-import ManageData from './ManageData';
-export default function TaskManager() {
-//  stored data 
+import HeaderApp from './HeaderApp';
+
+export default function EditTask() {
+//for edit data destrucring and edit all data http://localhost:8000/added-task
 const[data,setData]=useState([]);
+const Navigate=useNavigate();
+const {id}=useParams();
+
 const taskname=useRef("");
 const priority=useRef("");
 const added_date=useRef("");
 const details=useRef("")
 const status=useRef("")
+// edit data
+useEffect(()=>{
+ axios.get(`http://localhost:8000/added-task/${id}`).then((response)=>{
+  
+    taskname.current.value=response.data.taskname;
+    details.current.value=response.data.details
+    priority.current.value=response.data.priority;
+    status.current.value=response.data.status
+    added_date.current.value=response.data.added_date;
+    
+ });
 
-// create a formHandeler
-const AddTaskHendeler=(e)=>{
-    e.preventDefault();
-    var insert={
+},[]);
+
+// update data create a FormHandeler
+const updFormHandeler=()=>{
+    var upd={
         taskname:taskname.current.value,
         details:details.current.value,
         priority:priority.current.value,
         status:status.current.value,
         added_date:added_date.current.value
-
-        
     }
 
-    // call api to add data using axios.post()
-     axios.post(`http://localhost:8000/added-task`,insert).then(()=>{
-        //   setData(response.data);
-        console.log(data);
-        // pass a message
+    // update data call api 
+    axios.put(`http://localhost:8000/added-task/${id}`,upd).then(()=>{
+        // pass a update message
         Swal.fire({
             title: "wow",
-            text: "Your Task successfully created",
+            text: "Your Task updated  successfully",
             icon: "success"
           });
 
-     },[]);
-
-     e.target.reset();
+          Navigate('/');
+    })
 }
+
+
   return (
     <div>
-      {/* add to list */}
+    <HeaderApp />
       <Container className="p-5 mt-5">
-        <h1>Add New Tasks Here</h1>
-       <Form onSubmit={AddTaskHendeler}>
+       <Form>
         <div className='form-group mt-3'>
             <label className='bi bi-pencil'>Todo</label>
             <input type='text' ref={taskname} placeholder='Write your task....' className='form-control' />
@@ -68,8 +81,8 @@ const AddTaskHendeler=(e)=>{
             <label className='bi bi-watch'>Status</label>
             <select ref={status}  placeholder='enter task status' className='form-control'>
                 <option value="">-select status-</option>
-                <option value="Pending">Pending</option>
-                <option value="Completed">Completed</option>
+                <option value="High">Pending</option>
+                <option value="Low">Completed</option>
 
             </select>
         </div> 
@@ -81,12 +94,12 @@ const AddTaskHendeler=(e)=>{
 
         <div className='form-group mt-3'>
            
-            <input type='submit' value="Save" placeholder='Write your task....' className='btn btn-sm btn-success' />
+            <button type='button' onClick={updFormHandeler}  className='btn btn-sm btn-success' >Update Task</button>
         </div> 
        </Form>
-       {/* display list */}
-       <ManageData />        
+          
       </Container>
+      
     </div>
   )
 }
